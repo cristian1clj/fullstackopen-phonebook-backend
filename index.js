5255558359697dde9f2eb2e2bb1cec8b5d3b2dff
cristian1clj <cristian1clj@gmail.com>
2024-05-28 12:28:01 -0500

    3.8

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan((tokens, req, res) => {
+  const msgTokens = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ]
+
+  if (tokens.method(req, res) === 'POST'){
+    msgTokens.push(tokens.body(req, res))
+  }
+
+  return msgTokens.join(' ')
+}))
 
 let persons = [
   { 
@@ -37,30 +53,30 @@ const generateId = () => {
   : newId
 }
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
   
   if (person){
-    response.json(person)
+    res.json(person)
   } else {
-    response.status(404).end()
+    res.status(404).end()
   }
 })
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post('/api/persons', (req, res) => {
+  const body = req.body
 
   if (!body.name || !body.number || body.name === '' || body.number === ''){
-    return response.status(400).json({
+    return res.status(400).json({
       error: "content missing"
     })
   } else if (persons.some(person => person.name === body.name)){
-    return response.status(409).json({
+    return res.status(409).json({
       error: 'name must be unique'
     })
   }
@@ -72,18 +88,18 @@ app.post('/api/persons', (request, response) => {
   }
 
   persons = persons.concat(newPerson)
-  response.json(persons)
+  res.json(persons)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  res.status(204).end()
 })
 
-app.get('/api/info', (request, response) => {
+app.get('/api/info', (req, res) => {
   const dateRequest = new Date().toUTCString()
-  response.send(`
+  res.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${dateRequest}</p>
   `)
